8b854f4e3e627622395e451c4898a98aa7044910
weeklyn <162134112+weeklyn@users.noreply.github.com>
2024-05-06 21:39:32 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".guide__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+});
+
+var swiper = new Swiper(".feedback__swiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    freeMode: true,
+});
